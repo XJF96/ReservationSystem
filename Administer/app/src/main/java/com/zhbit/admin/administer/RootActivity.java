b5da1136866dbc3cb86db6ef167bb3e2206971a2
xjf <1601676446@qq.com>
2017-12-22 14:11:41 +0800

    管理

---
+++ Administer/app/src/main/java/com/zhbit/admin/administer/RootActivity.java
@@ -0,0 +1,13 @@
+package com.zhbit.admin.administer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RootActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
